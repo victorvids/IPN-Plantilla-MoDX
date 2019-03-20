97caa8e089a18634e08acbd90da29a1f591c5178
Jonathan Molina <jonathankbs@gmail.com>
2019-03-20 09:50:14 -0600

    Se crea carpeta de ejericcios
    Se sube el código base de los ejercicios trabajados en la capacitación

---
+++ ejercicios/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    $tpl = array(
+         "titulo"    => "Plantilla"
+        ,"contenido" => "_contenido/_".basename(__FILE__)
+    );
+
+    $_n = "";
+
+    include "_tpl/plantilla.tpl.php";
+
+?>
